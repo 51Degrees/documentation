c3aae129e4d0b83d1bb533ca9773872bb7bb040c
Ben51Degrees <ben@51degrees.com>
2019-07-19 17:32:23 +0100

    DOC: Updated link style and moved to the right.

--- examplegrabber.js
+++ examplegrabber.js
@@ -28,7 +28,7 @@ function updateLinks(project, divId) {
 
 function addLink(url, divId) {
     var div = $('#' + divId);
-    var link = "<p><a href=\"" + url + "\" class=\"b-link--dotted\">Go to original page...</a></p>";
+    var link = "<div style=\"text-align:right;\"><a href=\"" + url + "\" class=\"b-link--dotted\">Go to original page...</a></div>";
     div.html(link + div.html());
 }
 
