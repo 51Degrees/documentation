58c047bc518338eec30dbabb9a52f8502a3c0086
51Degrees <CI@51degrees.com>
2020-05-12 15:19:11 +0000

    DOC: Regenerated HTML documentation pages.

--- 4.1/_info__index.js
+++ 4.1/_info__index.js
@@ -1,5 +1,6 @@
 var _info__index =
 [
     [ "Feature Matrix", "_info__feature_matrix.html", null ],
+    [ "Version Support", "_info__version_support.html", null ],
     [ "Frequently Asked Questions", "_info__f_a_qs.html", null ]
 ];
