10e2bd7ab862bb6e1754b66667135fb9b502d3e9
51Degrees <CI@51degrees.com>
2020-05-21 14:53:50 +0000

    DOC: Regenerated HTML documentation pages.

--- 4.1/_examples__index.js
+++ 4.1/_examples__index.js
@@ -3,5 +3,9 @@ var _examples__index =
     [ "Examples with a custom flow element", null, "_examples__custom_element__index" ],
     [ "Device Detection Examples", null, "_examples__device_detection__index" ],
     [ "Reverse Geocoding", null, "_examples__reverse_geocoding__index" ],
-    [ "Web Integration", "_examples__web_integration.html", null ]
+    [ "Web Integration", "_examples__web_integration.html", null ],
+    [ "Result Caching", "_examples__caching.html", null ],
+    [ "Combining Services", "_examples__combining_services.html", null ],
+    [ "Custom Cache Implementation", "_examples__custom_cache.html", null ],
+    [ "Property Accessors", "_examples__property_accessors.html", null ]
 ];
