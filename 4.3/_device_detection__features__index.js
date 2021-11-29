ab8f7cfb8e78856d305c3bd80abea4515ce68d2c
51Degrees <CI@51degrees.com>
2021-11-29 17:16:25 +0000

    DOC: Regenerated HTML documentation pages.

--- 4.3/_device_detection__features__index.js
+++ 4.3/_device_detection__features__index.js
@@ -3,5 +3,6 @@ var _device_detection__features__index =
     [ "False Positives", "_device_detection__features__false_positive_control.html", null ],
     [ "Native Key Lookup", "_device_detection__features__native_key_lookup.html", null ],
     [ "Performance Options", "_device_detection__features__performance_options.html", null ],
+    [ "Results for Apple Devices", "_device_detection__features__apple_device_table.html", null ],
     [ "TAC Lookup", "_device_detection__features__tac_lookup.html", null ]
 ];
