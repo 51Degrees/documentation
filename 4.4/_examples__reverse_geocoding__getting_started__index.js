eda7aa9291b3cef2527a5aaf9b8df89d471df695
51Degrees <CI@51degrees.com>
2022-09-01 11:50:02 +0000

    DOC: Regenerated HTML documentation pages.

--- 4.4/_examples__reverse_geocoding__getting_started__index.js
+++ 4.4/_examples__reverse_geocoding__getting_started__index.js
@@ -1,5 +1,4 @@
 var _examples__reverse_geocoding__getting_started__index =
 [
-    [ "51Degrees Cloud", "_examples__reverse_geocoding__getting_started_51_degrees_cloud.html", null ],
-    [ "On-premise Nominatim", "_examples__reverse_geocoding__getting_started__on_premise_nominatim.html", null ]
+    [ "51Degrees Cloud", "_examples__reverse_geocoding__getting_started_51_degrees_cloud.html", null ]
 ];
