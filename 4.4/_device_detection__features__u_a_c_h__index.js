7e4602397d908b4d13ee8713c5afee11b0a19102
51Degrees <CI@51degrees.com>
2023-03-06 13:06:13 +0000

    DOC: Regenerated HTML documentation pages.

---
+++ 4.4/_device_detection__features__u_a_c_h__index.js
@@ -0,0 +1,7 @@
+var _device_detection__features__u_a_c_h__index =
+[
+    [ "Overview", "_device_detection__features__u_a_c_h__overview.html", null ],
+    [ "HTTP Headers", "_device_detection__features__u_a_c_h__headers.html", null ],
+    [ "JavaScript", "_device_detection__features__u_a_c_h__javascript.html", null ],
+    [ "Required UA-CH Headers", "_device_detection__features__u_a_c_h__required_uach_headers.html", null ]
+];
