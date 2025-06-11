c730171f104a0a43c558b9538c56b9cf636ada2d
Automation51D <51DCI@51Degrees.com>
2025-06-11 15:03:59 +0000

    Update documentation from version/4.5

--- 4.5/_product_summaries__benchmarks__index.js
+++
@@ -1,5 +0,0 @@
-var _product_summaries__benchmarks__index =
-[
-    [ "Device Detection Benchmarks", "_product_summaries__benchmarks__device_detection.html", null ],
-    [ "IP Intelligence Benchmarks", "_product_summaries__benchmarks__ip_intelligence.html", null ]
-];
--- 4.5/_product_summaries__index.js
+++ 4.5/_product_summaries__index.js
@@ -1,6 +1,6 @@
 var _product_summaries__index =
 [
-    [ "Benchmarks", null, "_product_summaries__benchmarks__index" ],
+    [ "Benchmarks", "_product_summaries__benchmarks.html", null ],
     [ "Feature Matrix", "_product_summaries__feature_matrix.html", null ],
     [ "Tested Versions", "_product_summaries__tested_versions.html", null ]
 ];
