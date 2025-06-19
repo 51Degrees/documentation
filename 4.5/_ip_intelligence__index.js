c5d8ae2803f2a986c3212fa27b94104ec56c4039
Automation51D <51DCI@51Degrees.com>
2025-06-19 16:21:18 +0000

    Update documentation from version/4.5

--- 4.5/_ip_intelligence__index.js
+++ 4.5/_ip_intelligence__index.js
@@ -2,6 +2,7 @@ var _ip_intelligence__index =
 [
     [ "IP Intelligence Overview", "_ip_intelligence__overview.html", null ],
     [ "IP Intelligence Quick Start", "_ip_intelligence__quickstart.html", null ],
+    [ "IP Intelligence Algorithm", "_ip_intelligence__algorithm.html", null ],
     [ "IP Intelligence Features", null, "_ip_intelligence__features__index" ],
     [ "IP Intelligence Examples", null, "_ip_intelligence__examples__index" ]
 ];
