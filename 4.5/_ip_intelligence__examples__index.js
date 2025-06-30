99038d185a1c8ca54fafde1c9a14f54f7e0a44fd
Automation51D <51DCI@51Degrees.com>
2025-06-30 17:20:29 +0000

    Update documentation from version/4.5

--- 4.5/_ip_intelligence__examples__index.js
+++ 4.5/_ip_intelligence__examples__index.js
@@ -1,5 +1,9 @@
 var _ip_intelligence__examples__index =
 [
     [ "Getting Started - Console", null, "_ip_intelligence__examples__getting_started__console__index" ],
-    [ "Getting Started - Web", null, "_ip_intelligence__examples__getting_started__web__index" ]
+    [ "Getting Started - Web", null, "_ip_intelligence__examples__getting_started__web__index" ],
+    [ "Data File Updates", "_ip_intelligence__examples__data_file_updates.html", null ],
+    [ "Metadata", "_ip_intelligence__examples__metadata.html", null ],
+    [ "Offline Processing", "_ip_intelligence__examples__offline_processing.html", null ],
+    [ "Performance", "_ip_intelligence__examples__performance.html", null ]
 ];
