bbe4860ac7b1b00e92e624bb0e4cd3317994c287
Automation51D <51DCI@51Degrees.com>
2025-07-30 01:33:03 +0000

    Update documentation from version/4.5

--- 4.5/_reverse_geocoding__examples__getting_started__index.js
+++
@@ -1,4 +0,0 @@
-var _reverse_geocoding__examples__getting_started__index =
-[
-    [ "Reverse Geocoding Getting Started", "_reverse_geocoding__examples__getting_started_51_degrees_cloud.html", null ]
-];
--- 4.5/_reverse_geocoding__examples__index.js
+++ 4.5/_reverse_geocoding__examples__index.js
@@ -1,6 +1,6 @@
 var _reverse_geocoding__examples__index =
 [
-    [ "Getting Started", null, "_reverse_geocoding__examples__getting_started__index" ],
-    [ "Configure From File", null, "_reverse_geocoding__examples__configure_from_file__index" ],
-    [ "Reverse Geocoding Web Integration", null, "_reverse_geocoding__examples__web_integration__index" ]
+    [ "Reverse Geocoding Getting Started", "_reverse_geocoding__examples__getting_started_51_degrees_cloud.html", null ],
+    [ "Reverse Geocoding Configuration File", "_reverse_geocoding__examples__configure_from_file_51_degrees_cloud.html", null ],
+    [ "Reverse Geocoding Web Integration Examples", "_reverse_geocoding__examples__web_integration__examples.html", null ]
 ];
