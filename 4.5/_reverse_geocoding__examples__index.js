596423075fcc866f39f94d45aec734a829c4a803
Automation51D <CI@51Degrees.com>
2025-08-29 01:34:47 +0000

    Update documentation for version 4.5

--- 4.5/_reverse_geocoding__examples__index.js
+++ 4.5/_reverse_geocoding__examples__index.js
@@ -2,5 +2,6 @@ var _reverse_geocoding__examples__index =
 [
     [ "Reverse Geocoding Getting Started", "_reverse_geocoding__examples__getting_started_51_degrees_cloud.html", null ],
     [ "Reverse Geocoding Configuration File", "_reverse_geocoding__examples__configure_from_file_51_degrees_cloud.html", null ],
-    [ "Reverse Geocoding Web Integration Examples", "_reverse_geocoding__examples__web_integration__examples.html", null ]
+    [ "Reverse Geocoding Web Integration Examples", "_reverse_geocoding__examples__web_integration__examples.html", null ],
+    [ "Combined Device Detection + Reverse Geocoding", "_reverse_geocoding__examples__combining_services.html", null ]
 ];
