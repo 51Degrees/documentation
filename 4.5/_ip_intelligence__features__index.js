d573323f62248c5f0b232316be8900d7234a3c8e
Automation51D <CI@51Degrees.com>
2025-09-10 10:34:51 +0000

    Update documentation for version 4.5

--- 4.5/_ip_intelligence__features__index.js
+++ 4.5/_ip_intelligence__features__index.js
@@ -1,5 +1,8 @@
 var _ip_intelligence__features__index =
 [
+    [ "Confidence", "_ip_intelligence__features__confidence.html", null ],
+    [ "Area", "_ip_intelligence__features__area.html", null ],
+    [ "Human", "_ip_intelligence__features__human.html", null ],
     [ "Weighting", "_ip_intelligence__features__weighting.html", null ],
     [ "Randomization", "_ip_intelligence__features__randomization.html", null ]
 ];
